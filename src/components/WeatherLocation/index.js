fbe8c86ed1705cb9f9f62f6d5cf1052421dba1c1
Elbis Flores <elbismanuel@gmail.com>
2019-03-28 08:27:13 -0500

    Add data in WeatherData component

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,10 +4,27 @@ import WeatherData from './WeatherData';
 import PropTypes from 'prop-types';
 import './styles.css';
 
+//Importación de constantes
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY,
+} from '../../constants/weather'
+
+const data = {
+    temperature: 27,
+    weatherState : CLOUDY,
+    humidity: 87,
+    wind: "15 m/s"
+}
+
 const WeatherLocation = () => (
     <div className="weatherLocationCont"> 
         <Location city={"Ciudad de Panamá"} /> 
-        <WeatherData />
+        <WeatherData data={data} />
     </div>
 )
 
